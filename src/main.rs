cbb9538a8825f9c5a129e791422b03cd761bf3f2
Mike <mike.in.flames@gmail.com>
2024-11-17 16:45:02 +0100

    Refactor day 1: extract single function.

--- src/main.rs
+++ src/main.rs
@@ -1,36 +1,30 @@
 use std::fs;
 
+fn day01_analyze_floors(input: &str) -> (i32, Option<usize>) {
+    let mut floor = 0;
+    let mut first_basement_pos = None;
+
+    for (i, ch) in input.chars().enumerate() {
+        floor += match ch {
+          '(' => 1,
+          ')' => -1,
+          _ => 0  
+        };
+
+        if floor == -1 && first_basement_pos == None {
+            first_basement_pos = Some(i+1);
+        }
+    }
+
+    (floor, first_basement_pos)
+}
+
 fn main() {
     println!("AOC 2015 day 1");
 
     let input = fs::read_to_string("input.txt").expect("Failed to read input file.");
-
-    let mut floor: i32 = 0;
-    for ch in input.chars() {
-        floor += match ch {
-            '(' => 1,
-            ')' => -1,
-            _ => 0,
-        };
-    }
-
-    println!("Final floor: {}", floor);
-
-    let input2 = "()())";
-    floor = 0;
-    let mut pos = 0;
-    for (i, ch) in input.chars().enumerate() {
-        floor += match ch {
-            '(' => 1,
-            ')' => -1,
-            _ => 0,
-        };
-        if floor == -1 {
-            pos = i+1;
-            break;
-        }
-    }
-
-    println!("Basement position: {}", pos);
+    let (final_floor, first_basement_pos) = day01_analyze_floors(&input);
+    println!("Final floor: {}", final_floor);
+    println!("First basement position: {}", first_basement_pos.unwrap());
 
 }
