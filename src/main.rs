302b4000d4bbbb0e745d4a173aea2da4629bed17
Mike <mike.in.flames@gmail.com>
2024-11-17 16:20:28 +0100

    Solve day 1 part 1.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
+use std::fs;
+
 fn main() {
-    println!("Hello, world!");
+    println!("AOC 2015 day 1");
+
+    let input = fs::read_to_string("input.txt").expect("Failed to read input file.");
+
+    let mut floor: i32 = 0;
+    for ch in input.chars() {
+        floor += match ch {
+            '(' => 1,
+            ')' => -1,
+            _ => 0,
+        };
+    }
+
+    println!("Final floor: {}", floor);
+
 }
