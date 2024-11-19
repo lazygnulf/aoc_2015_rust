82c87d4eed33e86432fbb2387c4e80f70e640c9a
Mike <mike.in.flames@gmail.com>
2024-11-19 23:22:43 +0100

    Pull out day3 code in separate module. Add tests.

---
+++ src/day03/mod.rs
@@ -0,0 +1,88 @@
+use std::collections::HashMap;
+use std::fs;
+
+pub fn day03_part1() {
+    println!("AOC 2015 day 3 - part 1");
+
+    let input = fs::read_to_string("input_03.txt").expect("Failed to read input file.");
+
+    println!("Houses delivered: {}", solve_part_1(&input));
+}
+
+fn solve_part_1(input: &str) -> usize {
+    let mut house_visited = HashMap::new();
+    let mut pos = (0, 0);
+    house_visited.insert(pos, 1);
+
+    for ch in input.chars() {
+        pos = move_pos(pos, ch);
+        match house_visited.get(&pos) {
+            Some(x) => house_visited.insert(pos, x + 1),
+            None => house_visited.insert(pos, 1),
+        };
+    }
+
+    house_visited.len()
+}
+
+pub fn day03_part2() {
+    println!("AOC 2015 day 3 - part 2");
+
+    let input = fs::read_to_string("input_03.txt").expect("Failed to read input file.");
+
+    println!("Houses delivered: {}", solve_part_2(&input));
+}
+
+fn solve_part_2(input: &str) -> usize {
+    let mut house_visited = HashMap::new();
+    let mut pos = (0, 0);
+    let mut robo = (0, 0);
+    let mut robo_turn = false;
+    house_visited.insert(pos, 2);
+
+    for ch in input.chars() {
+        if robo_turn {
+            robo = move_pos(robo, ch);
+            match house_visited.get(&robo) {
+                Some(x) => house_visited.insert(robo, x + 1),
+                None => house_visited.insert(robo, 1),
+            };
+        } else {
+            pos = move_pos(pos, ch);
+            match house_visited.get(&pos) {
+                Some(x) => house_visited.insert(pos, x + 1),
+                None => house_visited.insert(pos, 1),
+            };
+        }
+        robo_turn = !robo_turn;
+    }
+
+    house_visited.len()
+}
+
+fn move_pos(pos: (i32, i32), dir: char) -> (i32, i32) {
+    match dir {
+        '>' => (pos.0 + 1, pos.1),
+        '<' => (pos.0 - 1, pos.1),
+        '^' => (pos.0, pos.1 - 1),
+        'v' => (pos.0, pos.1 + 1),
+        _ => pos,
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_part1_input() {
+        let input = fs::read_to_string("input_03.txt").expect("Failed to read input file.");
+        assert_eq!(solve_part_1(&input), 2081);
+    }
+
+    #[test]
+    fn test_part2_input() {
+        let input = fs::read_to_string("input_03.txt").expect("Failed to read input file.");
+        assert_eq!(solve_part_2(&input), 2341);
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
-use std::collections::HashMap;
 use std::fs;
+
+mod day03;
 
 fn day01_analyze_floors(input: &str) -> (i32, Option<usize>) {
     let mut floor = 0;
@@ -7,27 +8,17 @@ fn day01_analyze_floors(input: &str) -> (i32, Option<usize>) {
 
     for (i, ch) in input.chars().enumerate() {
         floor += match ch {
-          '(' => 1,
-          ')' => -1,
-          _ => 0  
+            '(' => 1,
+            ')' => -1,
+            _ => 0,
         };
 
         if floor == -1 && first_basement_pos == None {
-            first_basement_pos = Some(i+1);
+            first_basement_pos = Some(i + 1);
         }
     }
 
     (floor, first_basement_pos)
-}
-
-fn move_pos(pos: (i32, i32), dir: char) -> (i32, i32) {
-    match dir {
-        '>' => (pos.0+1, pos.1),
-        '<' => (pos.0-1, pos.1),
-        '^' => (pos.0, pos.1-1),
-        'v' => (pos.0, pos.1+1),
-        _ => pos 
-     }
 }
 
 fn main() {
@@ -37,7 +28,6 @@ fn main() {
     let (final_floor, first_basement_pos) = day01_analyze_floors(&input);
     println!("Final floor: {}", final_floor);
     println!("First basement position: {}", first_basement_pos.unwrap());
-
 
     println!("AOC 2015 day 2");
 
@@ -49,51 +39,19 @@ fn main() {
 
         let l = present[0];
         let w = present[1];
-        let h  = present[2];
+        let h = present[2];
 
-        let sides = [l*w, l*h, w*h];
-        wrapping_paper += 2*sides[0] + 2*sides[1] + 2*sides[2] + sides.iter().min().unwrap();
+        let sides = [l * w, l * h, w * h];
+        wrapping_paper += 2 * sides[0] + 2 * sides[1] + 2 * sides[2] + sides.iter().min().unwrap();
 
-        let perimeters = [2*(l+w), 2*(l+h), 2*(w+h)];
-        ribbon += perimeters.iter().min().unwrap() + l*w*h;
-
-
+        let perimeters = [2 * (l + w), 2 * (l + h), 2 * (w + h)];
+        ribbon += perimeters.iter().min().unwrap() + l * w * h;
     }
     println!("Wrapping paper needed: {}", wrapping_paper);
     println!("Ribbon needed: {}", ribbon);
 
-
-    println!("AOC 2015 day 3");
-
-    input = fs::read_to_string("input_03.txt").expect("Failed to read input file.");
-    let mut house_visited = HashMap::new();
-    let mut pos = (0, 0);
-    let mut robo = (0, 0);
-    let mut robo_turn = false;
-    house_visited.insert(pos, 2);
-
-    for ch in input.chars() {
-        if robo_turn {
-            robo = move_pos(robo, ch);
-            match house_visited.get(&robo) {
-                Some(x) => house_visited.insert(robo, x+1),
-                None => house_visited.insert(robo, 1)
-            };
-            
-        }
-        else {
-            pos = move_pos(pos, ch);
-            match house_visited.get(&pos) {
-                Some(x) => house_visited.insert(pos, x+1),
-                None => house_visited.insert(pos, 1)
-            };
-        }
-        robo_turn = !robo_turn;
-    }
-
-    println!("Houses delivered: {}", house_visited.len());
-
-
+    day03::day03_part1();
+    day03::day03_part2();
 }
 
 #[cfg(test)]
